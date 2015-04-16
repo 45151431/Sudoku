cb374d539fb6ca4e3b9e46824eea82f75fac387a
45151431 <f74036475@mail.ncku.edu.tw>
2015-04-16 11:48:49 +0800

    加了註解的版本

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -18,7 +18,7 @@ int ques_init[12][12]={0, 2, 6, 0, 7, 3, 0, 5, 1, -1, -1, -1,
                        6, 9, 0, 0, 1, 7, -1, -1, -1, 0, 8, 4,
                        3, 1, 2, -1, -1, -1, 7, 0, 0, 5, 0, 9,
                        7, 4, 8, -1, -1, -1, 0, 6, 9, 3, 0, 0,
-                       0, 0, 0, -1, -1, -1, 3, 1, 0, 7, 0, 8};
+                       0, 0, 0, -1, -1, -1, 3, 1, 0, 7, 0, 8};//初始的Sudoku
 
 Sudoku::Sudoku(){}
 void Sudoku::GiveQuestion()
@@ -36,7 +36,7 @@ void Sudoku::GiveQuestion()
 	num_change();
 	ran=rand();
 	num_change();
-    for(int j=0; j<12; j++)
+    for(int j=0; j<12; j++)//output question
     {
         printf("%d", ques[0][j]);
         for(int i=1; i<12; i++)
@@ -45,7 +45,7 @@ void Sudoku::GiveQuestion()
     }
     return;
 }
-void Sudoku::rotate()
+void Sudoku::rotate()//根據random值做轉0度,90度,180度,270度
 {
     int tmp=ran%4;
     if(tmp==0)
@@ -66,14 +66,14 @@ void Sudoku::rotate()
                 ques[i][j]=ques_init[j][11-i];
     return;
 }
-void Sudoku::change(int i1, int j1, int i2, int j2)
+void Sudoku::change(int i1, int j1, int i2, int j2)//設置一個函數方便我做交換
 {
     int tmp=ques[i1][j1];
     ques[i1][j1]=ques[i2][j2];
     ques[i2][j2]=tmp;
     return;
 }
-void Sudoku::column_change()
+void Sudoku::column_change()//根據random值做大column間的交換
 {
     int tmp=ran%6;
     if(tmp==0)
@@ -102,7 +102,7 @@ void Sudoku::column_change()
 	            change(i, j, i+3, j);
     return;
 }
-void Sudoku::row_change()
+void Sudoku::row_change()//根據random值做大row間的交換
 {
 	int tmp=ran%3;
     if(tmp==0)
@@ -131,7 +131,7 @@ void Sudoku::row_change()
             	change(i, j, i, j+3);
     return;
 }
-void Sudoku::num_change()
+void Sudoku::num_change()//根據random值做number間的交換
 {
     int tmp=ran, x, y;
     x=tmp%9+1;
@@ -160,14 +160,14 @@ void Sudoku::ReadIn()
             {
                 spa_c[space]=i;
                 spa_r[space]=j;
-				spa_b[space++]=i+2-(i%3)+12*(j+2-(j%3));
+				spa_b[space++]=i+2-(i%3)+12*(j+2-(j%3));//Read in時順便將空格所屬的的column,row,block存起來
             }
-            else if(Sudo[i][j]==-1)
+            else if(Sudo[i][j]==-1)//紀錄-1的總數
                 check++;
         }
 }
 /*******Solve part*******/
-int Sudoku::column_check(int c)
+int Sudoku::column_check(int c)//檢查column有沒有重複的數字
 {
     memset(flag, 0, sizeof(flag));
     for(int i=0; i<12; i++)
@@ -180,7 +180,7 @@ int Sudoku::column_check(int c)
     }
     return 0;
 }
-int Sudoku::row_check(int r)
+int Sudoku::row_check(int r)//檢查row有沒有重複的數字
 {
     memset(flag, 0, sizeof(flag));
     for(int i=0; i<12; i++)
@@ -193,7 +193,7 @@ int Sudoku::row_check(int r)
     }
     return 0;
 }
-int Sudoku::block_check(int c, int r)
+int Sudoku::block_check(int c, int r)//檢查block有沒有重複的數字
 {
     memset(flag, 0, sizeof(flag));
     for(int i=0; i<3; i++)
@@ -207,9 +207,9 @@ int Sudoku::block_check(int c, int r)
         }
     return 0;
 }
-void Sudoku::space_solve(int N)
+void Sudoku::space_solve(int N)//recursive解題function
 {
-    if(N==space)
+    if(N==space)//填寫完成後,做重複的檢查
     {
         for(int i=0; i<12; i++)
         {
@@ -233,27 +233,27 @@ void Sudoku::space_solve(int N)
         if(check!=-1)
         {
             ans_n++;
-            if(ans_n==1)
+            if(ans_n==1)//檢查後發現沒有重複的數字即為解,如果是第一個解,將它存下來
                 for(int i=0; i<12; i++)
                     for(int j=0; j<12; j++)
                         ans[i][j]=Sudo[i][j];
-        } 
+        }
         return;
     }
     for(int i=1; i<10; i++)
     {
-        if(pos[N][i]||pos_fill[N][i])
+        if(pos[N][i]||pos_fill[N][i])//本來就不能填的數字和前面填了導致不能填的數字都要continue
             continue;
-		for(int j=N+1; j<space; j++)
-			if(spa_c[N]==spa_c[j]||spa_r[N]==spa_r[j]||spa_b[N]==spa_b[j])
+		for(int j=N+1; j<space; j++)//前面的數字填寫後,影響後面數字的填寫
+			if(spa_c[N]==spa_c[j]||spa_r[N]==spa_r[j]||spa_b[N]==spa_b[j])//同column,row,block的會受影響
 			{
 				pos_fill[j][Sudo[spa_c[N]][spa_r[N]]]=0;
 				pos_fill[j][i]=1;
 			}
-        Sudo[spa_c[N]][spa_r[N]]=i;
-        space_solve(N+1);
-        if(ans_n>1)
-            break;
+        Sudo[spa_c[N]][spa_r[N]]=i;//將數字填入
+        space_solve(N+1);//recursive
+        if(ans_n>1)//ans_n>1代表發現重複解,將function終止
+            return;
     }
     return;
 }
@@ -287,7 +287,7 @@ void Sudoku::Solve()
         cout<<"0\n";
         return;
     }
-    for(int i=0; i<12; i++)//判斷行列中不能有重複數字
+    for(int i=0; i<12; i++)//判斷column,row中不能有重複數字
     {
         check=column_check(i);
         if(check==-1)
@@ -302,7 +302,7 @@ void Sudoku::Solve()
             return;
         }
     }
-    for(int i=2; i<12; i+=3)//判斷九宮格內不能有重複數字
+    for(int i=2; i<12; i+=3)//判斷block內不能有重複數字
         for(int j=2; j<12; j+=3)
         {
             if(Sudo[i][j]==-1)
@@ -320,7 +320,7 @@ void Sudoku::Solve()
         return;
     }
     memset(pos, 0, sizeof(pos));
-    for(int i=0; i<space; i++)
+    for(int i=0; i<space; i++)//判斷在原始題目下,空格能填的數字
     {
         int c=spa_c[i], r=spa_r[i];
         for(int j=0; j<12; j++)
@@ -345,14 +345,14 @@ void Sudoku::Solve()
             return;
         }
     }
-    ans_n=0;
+    ans_n=0;//將ans數歸0
 	memset(pos_fill, 0, sizeof(pos_fill));
-    space_solve(0);
-    if(ans_n==0)
+    space_solve(0);//呼叫解題的function
+    if(ans_n==0)//無解
         cout<<"0\n";
-    else if(ans_n>1)
+    else if(ans_n>1)//非唯一解
         cout<<"2\n";
-    else if(ans_n==1)
+    else if(ans_n==1)//唯一解
     {
         printf("1\n");
         for(int j=0; j<12; j++)
